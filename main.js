294e2ccfd7284bc39af722cb9f2cb1c80d593f09
aaisha26 <aaishaperveenkvs@gmail.com>
2023-07-04 13:14:02 +0530

    basic_js_continue

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("hellooooo")
+const person={
+    firstname: "aaisha",
+    lastName:"perveen",
+    age:19,
+}
+console.log(person.firstname)
+const iseven= 10%2 ===0 ? "number is even" : "number is odd"
+console.log(iseven)
+//implicit conversion
+console.log("4"+ 3)
+console.log(true+"2")
+console.log("2"-"4")
+console.log("5"*"2")
+console.log("5"-true)
+console.log(5-true)
+console.log(5+undefined)
+//explicit conversion
+console.log(Number(null))
+//conditional statements
